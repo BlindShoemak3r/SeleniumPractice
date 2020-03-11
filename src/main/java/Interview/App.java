07477fe29798489ee16d11f0e6fdd111dff9fdf0
blindshoemak3r <blindshoemak3r@gmail.com>
2020-03-11 17:45:00 -0400

    going to pound town update

--- src/main/java/Interview/App.java
+++ src/main/java/Interview/App.java
@@ -1,13 +1,15 @@
 package Interview;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args ) throws InterruptedException
     {
     	System.setProperty("webdriver.chrome.driver", 
     	"C:\\My Stuff\\Learning Selenium\\chromedriver.exe");
@@ -16,14 +18,20 @@ public class App
     	driver.get("https://www.google.com/");
     	driver.findElement(By.cssSelector("input[title='Search']"))
     		  .sendKeys("Frank Pound" + Keys.ENTER);
-    	driver.findElement(By.linkText("Frank Pound | Apollo - Apollo.io")).click();
-    	driver.findElement(By.xpath("//div[@id='rso']/div[3]/div/div[4]/div/div/div/a/h3")).click();
     	
-    
+    	JavascriptExecutor je = (JavascriptExecutor) driver;
+    	WebElement preceeding = driver.findElement(By.xpath("//*[@id='rso']/div[3]/div/div[2]/div/div/div[1]/a/h3"));
+    	WebElement apolloLink = driver.findElement(By.xpath("//*[@id='rso']/div[3]/div/div[3]/div/div/div[1]/a/h3"));
+    	je.executeScript("arguments[0].scrollIntoView(true)", preceeding);
+    	apolloLink.click();
+
+    	Thread.sleep(7000);
+
+    	WebElement invicta = driver.findElement(By.xpath("//*[@id=\"experience\"]/div[2]/div/div[5]/div/a/div/div[1]/div[2]/span"));
+    	je.executeScript("arguments[0].scrollIntoView(true)", invicta);
+    	WebElement marine = driver.findElement(By.xpath("//*[@id='experience']/div[2]/div/div[6]/div/a/div/div[1]/div[1]"));
+    	je.executeScript("arguments[0].setAttribute('style','background: yellow; border: 2px solid red;')", marine);
     	
-    	
-    	
-    	 
     }
 }
 
