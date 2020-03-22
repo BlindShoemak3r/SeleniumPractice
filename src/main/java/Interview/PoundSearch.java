bf24877ac4a57fe7ab3ed53badd8ee75155f325d
blindshoemak3r <blindshoemak3r@gmail.com>
2020-03-22 17:23:22 -0400

    Hashmap greatest value and order

--- src/main/java/Interview/PoundSearch.java
+++ src/main/java/Interview/PoundSearch.java
@@ -1,39 +1,118 @@
 package Interview;
 
+import java.io.File;
+import java.util.HashMap;
+import java.util.Map;
+
+import org.apache.commons.io.FileUtils;
 import org.openqa.selenium.By;
 import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class PoundSearch {
 
-	public static void main(String[] args) {
+	public static int i = 0;
+	public static String fileParth = "C:\\My Stuff\\Learning Selenium\\Screenshots\\PoundSearch\\pic";
+	public static String fileType = ".png";
+	
+	public static void main(String[] args) throws Exception {
 		System.setProperty("webdriver.chrome.driver", "C:\\My Stuff\\Learning Selenium\\chromedriver.exe");
 		WebDriver driver = new ChromeDriver();
 		
-		driver.get("https://www.google.com");
+		String googleWebpage = "https://www.google.com"; 
+		driver.get(googleWebpage);
 		driver.findElement(By.cssSelector("input[title='Search']")).sendKeys("Frank Pound" + Keys.ENTER);
+		takeSnapShot(driver, fileParth + i + fileType);
 		String googleText = driver.findElement(By.id("result-stats")).getText();
-		String googleRemoveText = googleText.replaceAll("[^0-9]+","");
-		String googleNumbers = googleRemoveText.substring(0, googleRemoveText.length() - 3);
-		int google = Integer.parseInt(googleNumbers);
-		System.out.println(google);
+		String googleRemoveText = googleText.substring(0, googleText.length() - 16);
+		int google = stringConvert(googleRemoveText);
 		
-		driver.get("https://www.bing.com");
+		String bingWebpage = "https://www.bing.com";
+		driver.get(bingWebpage);
 		driver.findElement(By.id("sb_form_q")).sendKeys("Frank Pound" + Keys.ENTER);
+		takeSnapShot(driver, fileParth + i + fileType);
 		String bingText = driver.findElement(By.className("sb_count")).getText();
-		String bingNumbers = bingText.replaceAll("[^0-9]+","");
-		int bing = Integer.parseInt(bingNumbers);
-		System.out.println(bing);
+		int bing = stringConvert(bingText);
 		
-		driver.get("https://www.wikipedia.org");
+		String wikiWebpage = "https://www.wikipedia.org";
+		driver.get(wikiWebpage);
 		driver.findElement(By.name("search")).sendKeys("Frank Pound" + Keys.ENTER);
+		takeSnapShot(driver, fileParth + i + fileType);
 		String wikiText = driver.findElement(By.xpath("//div[@class='results-info']/strong[2]")).getText();
-		String wikiNumbers = wikiText.replaceAll("[^0-9]+","");
-		int wiki = Integer.parseInt(wikiNumbers);
-		System.out.println(wiki);
+		int wiki = stringConvert(wikiText);
 		
+		HashMap<String, Integer> map = new HashMap<>(); 
+
+		map.put(googleWebpage, google); 
+		map.put(bingWebpage, bing); 
+		map.put(wikiWebpage, wiki);
+		 
+		Map.Entry<String, Integer> maxEntry = null; //stand alone entry
+
+		for (Map.Entry<String, Integer> entry : map.entrySet()){   //for every entry in this set of entries
+		    if (maxEntry == null || entry.getValue().compareTo(maxEntry.getValue()) >= 0) {
+		        
+		    	maxEntry = entry;
+		    }
+		}
+		String maxEntryWeb = maxEntry.getKey();
+		driver.get(maxEntryWeb);
+		
+		takeSnapShot(driver, fileParth + i + fileType);
+
 		driver.close();
+	}
+	
+	public static void takeSnapShot(WebDriver webdriver,String fileWithPath) throws Exception {
+    	TakesScreenshot scrShot =((TakesScreenshot)webdriver);
+    	File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+    	File DestFile=new File(fileWithPath);
+    	FileUtils.copyFile(SrcFile, DestFile);
+    	i++;
+    }
+	
+	public static int stringConvert(String numbers) {
+		numbers = numbers.replaceAll("[^0-9]+","");
+		return Integer.parseInt(numbers);
 	}
 
 }
+
+/*
+create hash map
+put values in a list
+get highest value
+get key of that value
+open the webpage of that value
+
+remove all text, but keep commas: replaceAll("[^0-9\\,]+",""); 
+ 
+driver.get("https://www.msn.com");
+driver.findElement(By.id("header-search-input")).sendKeys("Frank Pound" + Keys.ENTER);
+String yahoo = driver.findElement(By.xpath("//div[@class='compPagination']/span")).getText();
+System.out.println(yahoo); 
+ 
+ //int maxMap = (Collections.max(map.values()));
+ //Entry<String, int> maxEntry = Collections.max(map.entrySet(), )
+ /*map.entrySet().stream().max((entry1, entry2) -> entry1.getValue() > entry2.getValue() ? 1 : -1).get().getValue();
+ 
+ List listOfMax = map.entrySet().stream().filter(entry -> entry.getValue() == max).map(map.Entry::getKey).collect(Collectors.toList());
+ System.out.println(listOfMax);
+ 
+ 
+ Set set = map.entrySet();
+ Iterator iterator = set.iterator();
+ while(iterator.hasNext()) {
+	 Map.Entry me = (Map.Entry)iterator.next();
+	 System.out.print(me.getKey() + ": ");
+	 System.out.println(me.getValue());
+ }
+ 
+ 
+ 
+ 
+ 
+ */
---
+++ src/main/java/Interview/PoundSearchOrder.java
@@ -0,0 +1,130 @@
+package Interview;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map.Entry;
+import java.util.Set;
+import java.util.LinkedHashMap;
+
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class PoundSearchOrder {
+
+	public static void main(String[] args) throws Exception {
+		System.setProperty("webdriver.chrome.driver", "C:\\My Stuff\\Learning Selenium\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		
+		int i = 0;
+		driver.get("https://www.google.com");
+		driver.findElement(By.cssSelector("input[title='Search']")).sendKeys("Frank Pound" + Keys.ENTER);
+		takeSnapShot(driver, "C:\\My Stuff\\Learning Selenium\\Screenshots\\PoundSearch\\pic"+i+".png");
+		i++;
+		String googleText = driver.findElement(By.id("result-stats")).getText();
+		String googleRemoveText = googleText.replaceAll("[^0-9]+","");
+		String googleNumbers = googleRemoveText.substring(0, googleRemoveText.length() - 3);
+		int google = Integer.parseInt(googleNumbers);
+		
+		driver.get("https://www.bing.com");
+		driver.findElement(By.id("sb_form_q")).sendKeys("Frank Pound" + Keys.ENTER);
+		takeSnapShot(driver, "C:\\My Stuff\\Learning Selenium\\Screenshots\\PoundSearch\\pic"+i+".png");
+		i++;
+		String bingText = driver.findElement(By.className("sb_count")).getText();
+		String bingNumbers = bingText.replaceAll("[^0-9]+","");
+		int bing = Integer.parseInt(bingNumbers);
+		
+		driver.get("https://www.wikipedia.org");
+		driver.findElement(By.name("search")).sendKeys("Frank Pound" + Keys.ENTER);
+		takeSnapShot(driver, "C:\\My Stuff\\Learning Selenium\\Screenshots\\PoundSearch\\pic"+i+".png");
+		String wikiText = driver.findElement(By.xpath("//div[@class='results-info']/strong[2]")).getText();
+		String wikiNumbers = wikiText.replaceAll("[^0-9]+","");
+		int wiki = Integer.parseInt(wikiNumbers);
+		
+		 HashMap<String, Integer> map = new HashMap<>(); 
+
+		 map.put("Google", google); 
+		 map.put("Bing", bing); 
+		 map.put("Wikipedia", wiki);
+		 
+		 Set<Entry<String, Integer>> entries = map.entrySet();
+		 
+		 System.out.println("Unsorted List:");
+		 for (String s : map.keySet()) {
+			 System.out.println(s + ": " + map.get(s));
+		 }
+		 
+		 Comparator<Entry<String, Integer>> valueComparator = new Comparator<Entry<String, Integer>>() {
+			 @Override
+			 public int compare (Entry<String, Integer> e1, Entry<String, Integer> e2) {
+				 Integer v1 = e1.getValue();
+				 Integer v2 = e2.getValue();
+				 return v1.compareTo(v2);		//ascending order
+			   //return v2.compareTo(v1);		//descending order
+			 }
+		 };
+		 
+		 List<Entry<String, Integer>> listOfEntries = new ArrayList<Entry<String, Integer>>(entries); 
+		 Collections.sort(listOfEntries, valueComparator);
+		 LinkedHashMap<String, Integer> sortedByValue = new LinkedHashMap<String, Integer>(listOfEntries.size());
+		 
+		 for (Entry<String, Integer> entry : listOfEntries) {
+			 sortedByValue.put(entry.getKey(), entry.getValue());
+		 }
+		 
+		 Set<Entry<String, Integer>> entrySetSortedByValue = sortedByValue.entrySet();
+		 
+		 System.out.println("\n" + "Sorted List:");
+		 for(Entry<String, Integer> mapping : entrySetSortedByValue) { 
+			 System.out.println(mapping.getKey() + ": " + mapping.getValue()); 
+		 }
+
+		driver.close();
+	}
+	
+	public static void takeSnapShot(WebDriver webdriver,String fileWithPath) throws Exception {
+    	TakesScreenshot scrShot =((TakesScreenshot)webdriver);
+    	File SrcFile=scrShot.getScreenshotAs(OutputType.FILE);
+    	File DestFile=new File(fileWithPath);
+    	FileUtils.copyFile(SrcFile, DestFile);
+    }
+	
+
+}
+
+/*
+remove all text, but keep commas: replaceAll("[^0-9\\,]+",""); 
+ 
+driver.get("https://www.msn.com");
+driver.findElement(By.id("header-search-input")).sendKeys("Frank Pound" + Keys.ENTER);
+String yahoo = driver.findElement(By.xpath("//div[@class='compPagination']/span")).getText();
+System.out.println(yahoo); 
+ 
+ //int maxMap = (Collections.max(map.values()));
+ //Entry<String, int> maxEntry = Collections.max(map.entrySet(), )
+ /*map.entrySet().stream().max((entry1, entry2) -> entry1.getValue() > entry2.getValue() ? 1 : -1).get().getValue();
+ 
+ List listOfMax = map.entrySet().stream().filter(entry -> entry.getValue() == max).map(map.Entry::getKey).collect(Collectors.toList());
+ System.out.println(listOfMax);
+ 
+ 
+ Set set = map.entrySet();
+ Iterator iterator = set.iterator();
+ while(iterator.hasNext()) {
+	 Map.Entry me = (Map.Entry)iterator.next();
+	 System.out.print(me.getKey() + ": ");
+	 System.out.println(me.getValue());
+ }
+ 
+ 
+ 
+ 
+ 
+ */
